177a318cabd9ef1b777f99aaaa947b9ecaf31564
brunoqs <brun_o13@hotmail.com>
2017-07-13 21:22:11 -0300

    SplashScreen implementada

---
+++ app/src/main/java/com/example/emakers/tombanana/SplashScreen.java
@@ -0,0 +1,24 @@
+package com.example.emakers.tombanana;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity implements Runnable{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Handler handler = new Handler();
+        handler.postDelayed(this, 3000); // delay de 3 segundos
+    }
+
+    @Override
+    public void run(){
+        startActivity(new Intent(this, MainActivity.class)); // apos o delay chama a tela MainActivity
+        finish();
+    }
+}
